6f81e622953f889574e2302c35994a41930635ed
ncou <nicolas_courade@yahoo.fr>
2018-09-03 00:15:59 +0200

    update

--- tests/ViewPathTest.php
+++ tests/ViewPathTest.php
@@ -9,56 +9,23 @@ use Chiron\Views\ViewPath;
 
 class ViewPathTest extends TestCase
 {
-
-    // ****************************************
-    // *************** ASSERT *****************
-    // ****************************************
-
-    public function assertTemplatePath($path, ViewPath $templatePath, $message = null)
-    {
-        $message = $message ?: sprintf('Failed to assert ViewPath contained path %s', $path);
-        $this->assertEquals($path, $templatePath->getPath(), $message);
-    }
-    public function assertTemplatePathString($path, ViewPath $templatePath, $message = null)
-    {
-        $message = $message ?: sprintf('Failed to assert ViewPath casts to string path %s', $path);
-        $this->assertEquals($path, (string) $templatePath, $message);
-    }
-    public function assertTemplatePathNamespace($namespace, ViewPath $templatePath, $message = null)
-    {
-        $message = $message ?: sprintf(
-            'Failed to assert TemplatePath namespace matched %s',
-            var_export($namespace, true)
-        );
-        $this->assertEquals($namespace, $templatePath->getNamespace(), $message);
-    }
-    public function assertEmptyTemplatePathNamespace(ViewPath $templatePath, $message = null)
-    {
-        $message = $message ?: 'Failed to assert ViewPath namespace was empty';
-        $this->assertEmpty($templatePath->getNamespace(), $message);
-    }
-
-    // ***************************************
-    // *************** TESTS *****************
-    // ***************************************
-
     public function testCanProvideNamespaceAtInstantiation()
     {
         $templatePath = new ViewPath('/tmp', 'test');
-        $this->assertTemplatePath('/tmp', $templatePath);
-        $this->assertTemplatePathNamespace('test', $templatePath);
+        $this->assertEquals('/tmp', $templatePath->getPath());
+        $this->assertEquals('test', $templatePath->getNamespace());
     }
     public function testCanInstantiateWithoutANamespace()
     {
         $templatePath = new ViewPath('/tmp');
-        $this->assertTemplatePath('/tmp', $templatePath);
-        $this->assertEmptyTemplatePathNamespace($templatePath);
+        $this->assertEquals('/tmp', $templatePath->getPath());
+        $this->assertEmpty($templatePath->getNamespace());
     }
     public function testCastingToStringReturnsPathOnly()
     {
         $templatePath = new ViewPath('/tmp');
-        $this->assertTemplatePathString('/tmp', $templatePath);
+        $this->assertEquals('/tmp', (string) $templatePath);
         $templatePath = new ViewPath('/tmp', 'test');
-        $this->assertTemplatePathString('/tmp', $templatePath);
+        $this->assertEquals('/tmp', (string) $templatePath);
     }
 }
