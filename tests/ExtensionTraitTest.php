53b9f79b84016571b00c6f5167100259a8821e2b
ncou <nicolas_courade@yahoo.fr>
2018-10-15 18:08:13 +0200

    add some tests

---
+++ tests/ExtensionTraitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Tests\Views;
+
+use PHPUnit\Framework\TestCase;
+
+class ExtensionTraitTest extends TestCase
+{
+    /**
+     * Holds the Container instance for testing.
+     *
+     * @var \Chiron\Container\ContainerAwareTrait
+     */
+    protected $object;
+
+    /**
+     * Setup the tests.
+     */
+    protected function setUp()
+    {
+        $this->object = $this->getObjectForTrait('\\Chiron\\Views\\ExtensionTrait');
+    }
+
+    /**
+     * Tear down the tests.
+     */
+    protected function tearDown()
+    {
+        $this->object = null;
+    }
+
+    /**
+     * @coversDefaultClass  getExtension
+     */
+    public function testGetExtensionDefaultValue()
+    {
+        $this->assertEquals('html', $this->object->getExtension());
+    }
+
+    /**
+     * @coversDefaultClass  setExtension
+     */
+    /*
+    public function testSetExtension()
+    {
+        $this->object->setExtension('tpl');
+        $this->assertEquals('tpl', $this->object->getExtension());
+    }*/
+}
