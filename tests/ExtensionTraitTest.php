57cb9e0418b5fd3e58ea2d663a4477693dd408a9
ncou <nicolas_courade@yahoo.fr>
2018-10-15 16:11:15 +0000

    Apply fixes from StyleCI

--- tests/ExtensionTraitTest.php
+++ tests/ExtensionTraitTest.php
@@ -39,7 +39,7 @@ class ExtensionTraitTest extends TestCase
         $this->assertEquals('html', $this->object->getExtension());
     }
 
-    /**
+    /*
      * @coversDefaultClass  setExtension
      */
     /*
--- tests/TemplatePathTest.php
+++ tests/TemplatePathTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Chiron\Tests\Views;
 
-use PHPUnit\Framework\TestCase;
 use Chiron\Views\TemplatePath;
+use PHPUnit\Framework\TestCase;
 
 class TemplatePathTest extends TestCase
 {
@@ -15,12 +15,14 @@ class TemplatePathTest extends TestCase
         $this->assertEquals('/tmp', $templatePath->getPath());
         $this->assertEquals('test', $templatePath->getNamespace());
     }
+
     public function testCanInstantiateWithoutANamespace()
     {
         $templatePath = new TemplatePath('/tmp');
         $this->assertEquals('/tmp', $templatePath->getPath());
         $this->assertEmpty($templatePath->getNamespace());
     }
+
     public function testCastingToStringReturnsPathOnly()
     {
         $templatePath = new TemplatePath('/tmp');
